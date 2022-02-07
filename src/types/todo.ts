c2e74dc07426897aba13c01316d52ba6b0657ef0
Shu Shikata <shu.42111227@gmail.com>
2022-02-08 01:26:46 +0900

    型定義の実践

---
+++ src/types/todo.ts
@@ -0,0 +1,6 @@
+export type TodoType = {
+  title: string;
+  userId: number;
+  id: number;
+  completed: boolean;
+};
---
+++ src/types/user.ts
@@ -0,0 +1,4 @@
+export type User = {
+  name: string;
+  hobiies?: Array<string>;
+};
